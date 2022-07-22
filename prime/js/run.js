38cc0ad55c5c37e9e435236bba938f65a253f5bd
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-07-22 15:58:27 +0530

    Add files via upload

---
+++ prime/js/run.js
@@ -0,0 +1,16 @@
+var n=9;
+var a=0;
+for (var i=2;i<n;i++)
+{
+ 
+         if(n%i==0)
+      {
+         a=1;
+   }
+}
+if(a==0){
+         
+   console.log(n,'is a prime');
+}
+  else{
+   console.log(n,'is a not prime');}
